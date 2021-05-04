0481186ba68a56f61ea1445174714d66eae2a850
押切郁也 <fumi.topone@gmail.com>
2021-05-04 19:19:35 +0900

    first commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import locationReducer from '../features/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    location:locationReducer
   },
 });
 
